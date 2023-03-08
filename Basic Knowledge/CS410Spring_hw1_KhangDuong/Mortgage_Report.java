002f87a50a21e1ad05deac170b72eecf89a1f611
Đường Huỳnh Bảo Khang <b323a775@wichita.edu>
2023-03-08 00:28:08 -0600

    JAV class

---
+++ Basic Knowledge/CS410Spring_hw1_KhangDuong/Mortgage_Report.java
@@ -0,0 +1,26 @@
+package CS410Spring_hw1_KhangDuong;
+
+import java.text.NumberFormat;
+
+public class Mortgage_Report extends Mortgage {
+    Mortgage_Report(int principal, float annualInterest, byte years) {
+        super(principal, annualInterest, years);
+    }
+    public void printMortgage() {
+        double mortgage = calculateMortgage();
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println();
+        System.out.println("MORTGAGE");
+        System.out.println("--------");
+        System.out.println("Monthly Payments: " + mortgageFormatted);
+    }
+    public void printPaymentSchedule() {
+        System.out.println();
+        System.out.println("PAYMENT SCHEDULE");
+        System.out.println("----------------");
+        for (short month = 1; month <= years * MONTHS_IN_YEAR; month++) {
+            double balance = calculateBalance(month);
+            System.out.println(NumberFormat.getCurrencyInstance().format(balance));
+        }
+    }
+}
--- sechbunglo/test.cpp
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-#include<iostream>
-int main(){
-    std::cout << "sechbunglo";
-    return 0;
-}
